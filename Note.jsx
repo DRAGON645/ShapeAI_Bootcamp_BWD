e9092297a7781387fa6783532f69c9563ef3655c
DRAGON645 <79984670+DRAGON645@users.noreply.github.com>
2021-06-06 21:10:24 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+function Note() {
+   return (
+    <div className="note">
+      <h1>Valorent</h1>
+      <p>VALORANT is a free-to-play first-person tactical shooter video game being developed and published by Riot Games. The game currently runs on Unreal Engine 4.
+
+It was announced on October 15, 2019 under the codename "Project A", until it was officially revealed as VALORANT on March 2, 2020. It was officially released on June 2nd, 2020 for PC.</p>
+    </div>
+    ); 
+} 
+export default Note
